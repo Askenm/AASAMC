3f9106fad9e39881f6d79c4a97a562d2d35e0fba
DueViktor <viktorduep@gmail.com>
2022-12-12 19:02:18 +0100

    embedding creation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,70 +1,60 @@
 import json
 import numpy as np
-from tqdm import tqdm
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
 
 
-def load_data(path='../data/raw/train.json'):
-
-    with open(path,'r') as file:
+def load_data(path="../data/raw/train.json"):
+    with open(path, "r") as file:
         data = json.load(file)
-
     return data
+"""
+
+NOTE FROM VIKTOR: I CREATED A SEPERATE SCRIPT FOR EMBEDDING CREATION. IT CAN BE FOUND IN THE SRC FOLDER.
+TODO: DELETE THIS FILE
 
 
 
-def select_features(unk_tokens,threshold=3):
+def select_features(unk_tokens, threshold=3):
     ic = Counter(unk_tokens)
-    filtered = {k:v for k,v in dict(ic).items() if v >= threshold}
+    filtered = {k: v for k, v in dict(ic).items() if v >= threshold}
     return filtered
 
 
-
-def transform_corpus(data,threshold):
+def transform_corpus(data, threshold):
 
     ids = []
     labels = []
     corpus = []
     unk_tokens = []
     for datapoint in data:
-        unk_tokens+=datapoint['ingredients']
+        unk_tokens += datapoint["ingredients"]
 
-    filtered = select_features(unk_tokens,threshold=threshold)
+    filtered = select_features(unk_tokens, threshold=threshold)
 
     for datapoint in data:
         cont = True
         temp = []
-        for ing in datapoint['ingredients']:
-            temp.append(ing.replace(' ',''))
+        for ing in datapoint["ingredients"]:
+            temp.append(ing.replace(" ", ""))
             unk_tokens.append(ing)
             if ing not in filtered.keys():
                 cont = False
                 break
         if not cont:
             continue
-    
-        ids.append(datapoint['id'])
-        labels.append(datapoint['cuisine'])
-        corpus.append(' '.join(temp))
+
+        ids.append(datapoint["id"])
+        labels.append(datapoint["cuisine"])
+        corpus.append(" ".join(temp))
+
+    return labels, corpus
 
 
-
-    return labels,corpus
-
-
-
-
-
-    
-        
-
-
-    
-def vectorize_corpus(corpus,labels,save_path='../data/processed/onehot_representation.pkl'):
-
-
+def vectorize_corpus(
+    corpus, labels, save_path="../data/processed/onehot_representation.pkl"
+):
     # create the transform
     vectorizer = CountVectorizer()
     # tokenize and build vocab
@@ -77,24 +67,19 @@ def vectorize_corpus(corpus,labels,save_path='../data/processed/onehot_represent
     print(vector.shape)
     print(type(vector))
 
-
-
-    vector = np.c_[vector.T,np.asarray(labels)]
-    with open(save_path,'wb') as f:
+    vector = np.c_[vector.T, np.asarray(labels)]
+    with open(save_path, "wb") as f:
         pickle.dump(vector, f)
-
-
 
 
 def main():
     data = load_data()
-
-    labels,corpus = transform_corpus(data,threshold = 3)
-
-
-    vectorize_corpus(corpus,labels,save_path='../data/processed/onehot_representation.pkl')
-
+    labels, corpus = transform_corpus(data, threshold=3)
+    vectorize_corpus(
+        corpus, labels, save_path="../data/processed/onehot_representation.pkl"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+"""
