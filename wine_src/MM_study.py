bff478d9e1117f3faa11f13c56822609ab39cadc
Askenm <aske.osv@gmail.com>
2022-12-29 19:26:37 +0100

    segmented the monstrosity of a jupyter notebook into seperate codefiles

--- wine_src/MM_study.py
+++ wine_src/MM_study.py
@@ -3,8 +3,14 @@ import numpy as np
 from sklearn.datasets import load_wine
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import adjusted_mutual_info_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import silhouette_score, adjusted_rand_score, adjusted_mutual_info_score
+
+### FOLLOW git clone guide to install pomegranate
+# pip did not work for me
+# https://pomegranate.readthedocs.io/en/latest/install.html
+
+
 
 distributions = {"lognormal":LogNormalDistribution,"gamma":GammaDistribution,"normal":NormalDistribution}
 num_iters = 25
@@ -26,13 +32,12 @@ for iteration in range(num_iters):
     X_train, X_test, y_train, y_test = train_test_split(features,
                         labels,
                         test_size=0.2,
-                        #random_state=42,
                         stratify=labels)
-
 
     #scaler = StandardScaler()
     #X_train = scaler.fit_transform(X_train)
     #X_test = scaler.transform(X_test)
+
     for name, distribution in distributions.items():
 
         model = GeneralMixtureModel.from_samples(distribution, n_components=3,X=X_train)
---
+++ wine_src/feature_information.py
@@ -0,0 +1,47 @@
+from mrmr import mrmr_classif
+from sklearn.datasets import load_wine
+import pandas as pd
+from collections import defaultdict
+
+
+wine = load_wine()
+
+features, labels = wine.data, wine.target
+data_df = pd.DataFrame(wine.data, columns=wine.feature_names)
+data_df['target'] = wine.target
+
+
+
+feature_ranking = dict()
+ranked = []
+
+for i in range(1,14):
+    X = data_df[list(data_df.columns)[:-1]]
+    y = data_df[list(data_df.columns)[-1]]
+    selected_features = list(mrmr_classif(X=X, y=y, K=i))
+    print(feature_ranking.values())
+    print(selected_features)
+    if ranked:
+        for f in ranked:
+            selected_features.pop(selected_features.index(f))
+    
+    feature_ranking[i] = selected_features[0]
+    ranked+=selected_features
+    ranked = list(set(ranked))
+
+ranked_features = pd.DataFrame({'rank':feature_ranking.keys(),'feature':feature_ranking.values()})
+
+
+weighted_sum = defaultdict(int)
+dists_df = pd.read_csv('results/cluster_feature_distributions.csv')
+for k,row in dists_df.iterrows():
+    cur_rank = ranked_features.loc[ranked_features['feature']==row['feature']]['rank'].values[0]
+    weighted_sum[row['distribution']]+=cur_rank
+    
+    
+for k,count in weighted_sum.items():
+    weighted_sum[k] = count/dists_df.loc[dists_df.distribution == k].shape[0]
+    
+    
+pd.DataFrame({'distribution':list(weighted_sum.keys()),
+              'average_feature_information_rank':list(weighted_sum.values())}).to_csv('results/feature_information.csv',index=None)
