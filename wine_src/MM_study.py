e2d91f8e0f2dc832e09bf76d0723e99be1d83d1e
Askenm <aske.osv@gmail.com>
2022-12-29 15:48:06 +0100

    added mixture model study

---
+++ wine_src/MM_study.py
@@ -0,0 +1,46 @@
+from pomegranate import GeneralMixtureModel,LogNormalDistribution,GammaDistribution,NormalDistribution
+import numpy as np
+from sklearn.datasets import load_wine
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score, adjusted_rand_score, adjusted_mutual_info_score
+
+distributions = {"lognormal":LogNormalDistribution,"gamma":GammaDistribution,"normal":NormalDistribution}
+num_iters = 25
+
+
+wine = load_wine()
+features, labels = wine.data, wine.target
+data_df = pd.DataFrame(wine.data, columns=wine.feature_names)
+data_df['target'] = wine.target
+
+results = {'distribution':[],
+           'mutual_information':[]}
+
+for iteration in range(num_iters):
+    
+
+
+
+    X_train, X_test, y_train, y_test = train_test_split(features,
+                        labels,
+                        test_size=0.2,
+                        #random_state=42,
+                        stratify=labels)
+
+
+    #scaler = StandardScaler()
+    #X_train = scaler.fit_transform(X_train)
+    #X_test = scaler.transform(X_test)
+    for name, distribution in distributions.items():
+
+        model = GeneralMixtureModel.from_samples(distribution, n_components=3,X=X_train)
+        model.fit(X_train)
+        y_pred = model.predict(X_test)
+
+        results['distribution'].append(name)
+        results['mutual_information'].append(adjusted_mutual_info_score(y_test, y_pred))
+
+
+pd.DataFrame(results).groupby('distribution').mean().sort_values('mutual_information',ascending=False).to_csv('results/mixture_models.csv')
